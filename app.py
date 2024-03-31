f59f6e88613b3eadd7a60d1a01e3ebf76e2878a9
Teurus <darkozavr@gmail.com>
2024-03-31 14:59:20 +0500

    basic system

--- app.py
+++ app.py
@@ -28,38 +28,11 @@ def set_user_question_number(uid, value):
     rs.hset('users_question', mapping={uid: value})
 
 
-# def set_user_state(uid, value):
-#     rs.hset('users_state', mapping={uid: value})
-#     return True
-
-
-# def get_user_state(uid):
-#     users_state = rs.hgetall('users_state')
-#     if uid in users_state:
-#         return users_state[uid]
-#     else:
-#         rs.hset('users_state', mapping={uid: 0})
-#         return 0
-
-
-# def listener(messages):
-#     """
-#     When new messages arrive TeleBot will call this function.
-#     """
-#     for message in messages:
-#         if message.content_type == 'text':
-#             # print the sent message to the console
-#             print(str(message.chat.first_name) + " [" + str(message.chat.id) + "]: " + message.text)
-
-
-# bot.set_update_listener(listener)  # register listener
-
-
 @bot.message_handler(commands=['start', 'help'])
 def cmd_start(message: types.Message):
-    # cid = str(message.chat.id)
-    # if cid not in rs.hget('users_question', cid):
-    #     set_user_question_number(cid, 0)
+    cid = str(message.chat.id)
+    if not rs.hget('user_data', cid):
+        rs.hset('user_data', str(cid), '0000')
 
     markup = telebot.util.quick_markup({'Начать викторину!': {'callback_data': 'quiz'},
                                         'Информация': {'callback_data': 'opeka_info'}})
@@ -78,86 +51,126 @@ def cmd_reset(message: types.Message):
     cid = message.chat.id
     bot.send_message(cid, 'Хорошо! Давай начнем сначала.')
     set_user_question_number(cid, 0)
+    rs.hset('user_data', str(cid), '0000')
     next_question(message)
 
+
+@bot.message_handler(commands=['score'])  # ДЕБАГ КОМАНДА
+def dbg_score(message: types.Message):
+    bot.send_message(message.chat.id, message.from_user.username)
+    if message.from_user.username == 'Darkozavr':
+        var = rs.hget('user_data', str(message.chat.id))
+        bot.send_message(message.chat.id, var)
+
+
+@bot.message_handler(commands=['clear'])  # ДЕБАГ КОМАНДА
+def dbg_clear(message: types.Message):
+    bot.edit_message_reply_markup(chat_id=message.chat.id)
+    # ДОБАВИТЬ ЛИСТЕНЕР ДЛЯ УДАЛЕНИЯ ПРЕДЫДУЩИХ СООБЩЕНИЙ ПО ИХ АЙДИ
+    # ИЛИ УДАЛЯТЬ ПРИ ПЕРЕЗАПУСКЕ ВОПРОСОВ
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_handler(callback: types.CallbackQuery):
     bot.edit_message_reply_markup(chat_id=callback.message.chat.id,
                                   message_id=callback.message.message_id)
+    bot.delete_message()
+    cm = callback.message
     if callback.data == 'start':
-        # bot.answer_callback_query(callback.id, '<3')
         cmd_start(callback.message)
     elif callback.data == 'quiz':
         next_question(callback.message)
     elif callback.data == '1':
-        # Работа с параметрами ответа
-        # quiz(callback.message)
-        pass
+        after_answer_react(cm, 1)
     elif callback.data == '2':
-        # Работа с параметрами ответа
-        # quiz(callback.message)
-        pass
+        after_answer_react(cm, 2)
     elif callback.data == '3':
-        # Работа с параметрами ответа
-        # quiz(callback.message)
-        pass
+        after_answer_react(cm, 3)
     elif callback.data == '4':
-        # Работа с параметрами ответа
-        # quiz(callback.message)
-        pass
+        after_answer_react(cm, 4)
 
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message: types.Message):
     """Обработка для всех сообщений кроме команд"""
-    markup = telebot.util.quick_markup({'Начало!': {'callback_data': 'start'}})
-    if message.text not in [x.command for x in bot.get_my_commands()]:
+    cid = str(message.chat.id)
+    if not rs.hget('user_data', cid):
+        markup = telebot.util.quick_markup({'Начало!': {'callback_data': 'start'}})
         bot.send_message(message.chat.id,
                          'Не знаешь с чего начать? Попробуй кликнуть тут!',
                          reply_markup=markup)
+    else:
+        markup = telebot.util.quick_markup({'Начало!': {'callback_data': 'start'},
+                                   'К викторине': {'callback_data': 'quiz'}})
+        bot.send_message(message.chat.id,
+                         'Что то не так? Мы можем вернуться в начало или же '
+                         'к последнему вопросу викторины.',
+                         reply_markup=markup)
+
+    # if message.text not in [x.command for x in bot.get_my_commands()]:
+    #     bot.send_message(message.chat.id,
+    #                      'Не знаешь с чего начать? Попробуй кликнуть тут!',
+    #                      reply_markup=markup)
 
 
 def next_question(message: types.Message):
-    quiz = Quiz.quiz
+    quiz = Quiz.quiz.copy()
     cid = message.chat.id
-    quest_num = get_user_question_number(cid)
-    text = ''
-    try:
-        text = quiz[quest_num].get()
-    except Exception as e:
-        print('Something goes wrong \n', e)
-        pass
+    q_num = get_user_question_number(cid)
 
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    # добавить кнопки с ответами
+    if q_num >= len(quiz):
+        bot.send_message(message.chat.id, 'Это был последний вопрос, а сейчас посмотрим'
+                                          ' что у нас получилось :) Чтобы попробовать еще раз'
+                                          ' восользуйтесь командой /reset')
+        get_quiz_result(message)
+        return
+
+    text = ''
+    btns = {}
+    for question in quiz[q_num]:
+        text = question
+        num = 1
+        for answer in quiz[q_num][question]:
+            btns[answer] = {'callback_data': str(num)}
+            num += 1
+
+    markup = telebot.util.quick_markup(btns, row_width=2)
     bot.send_message(cid, text, reply_markup=markup)
 
-    # !!! Добавить проверку наличия или создание нового пользователя.
 
-    # for item in quiz:
-    #     markup = types.InlineKeyboardMarkup(row_width=2)
-    #     num = 1
-    #     q = ''
-    #     for question in item:
-    #         q = question
-    #         for answer_ in item[question]:
-    #             markup.add(types.InlineKeyboardButton(text=answer_, callback_data=str(num)))
-    #             num += 1
-    #     bot.send_message(message.chat.id, q, reply_markup=markup)
+def after_answer_react(message: types.Message, answer_num):
+    cid = message.chat.id
+    q_num = get_user_question_number(cid)
+    quiz = Quiz.quiz.copy()
+
+    if q_num >= len(quiz):
+        bot.send_message(cid, 'Ошибка')
+        return
+
+    for que in quiz[q_num]:
+        num = 1
+        for ans in quiz[q_num][que]:
+            if num != answer_num:
+                num += 1
+                continue
+
+            for par, val in quiz[q_num][que][ans]:
+                vars_ = int(rs.hget('user_data', str(cid)))
+                if par == 'group':
+                    if vars_ < 1000:
+                        vars_ += 1000
+                elif par == 'feature':
+                    if (0 <= vars_ - 1000 < 100) or (0 <= vars_ < 100):
+                        vars_ += 100
+                elif par == 'var':
+                    vars_ += val
+                rs.hset('user_data', str(cid), str(vars_))
+    set_user_question_number(cid, q_num + 1)
+    next_question(message)
 
 
 def get_quiz_result(message: types.Message):
-    pass
+    cid = message.chat.id
+    bot.send_message(cid, 'Победа! Картинка будет тут')  # ДЕБАГ ТЕКСТ
 
-
-# @bot.message_handler(func=lambda message: get_user_state(message.chat.id) == config.States.ANSWER.value)
-# def answer(message: types.Message):
-#     pass
-
-
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
 
 bot.polling()
